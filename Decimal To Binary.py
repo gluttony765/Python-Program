ef4cf8dd7aee6b453abc46e7d391a812d0f82cf1
rishabh-28 <rajatloomba312@gmail.com>
2020-10-02 16:11:58 +0530

    Using in-built bin function to convert a decimal to binary

--- Decimal To Binary.py
+++ Decimal To Binary.py
@@ -1,18 +1,2 @@
-def decToBinary(n): 
-      
-    # array to store 
-    # binary number 
-    binaryNum = [0] * n; 
-  
-    # counter for binary array 
-    i = 0; 
-    while (n > 0):  
-  
-        # storing remainder  
-        binaryNum[i] = n % 2; 
-        n = int(n / 2); 
-        i += 1;  
-  
-    # printing binary array 
-    for j in range(i - 1, -1, -1): 
-        print(binaryNum[j], end = ""); 
+def decimalToBinary(n):
+    print(bin(n).replace("0b",""))
