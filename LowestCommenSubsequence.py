8edf77b3bcd9d7126e9425c3e358a434d1977497
rishabh-28 <rajatloomba312@gmail.com>
2020-10-02 15:58:16 +0530

    Desired result is coming out now. Changes the code to much optimised DP algorithm

--- LowestCommenSubsequence.py
+++ LowestCommenSubsequence.py
@@ -1,20 +1,12 @@
 def lcs(X, Y): 
-    # find the length of the strings 
     m = len(X) 
-    n = len(Y) 
-  
-    # declaring the array for storing the dp values 
-    L = [[None]*(n - 1) for i in range(m - 1)] 
-  
-    for i in range(m + 1): 
-        for j in range(n + 1): 
-            if i == 0 or j == 0 : 
-                L[i][j] = 0
-            elif X[i-1] == Y[j-1]: 
-                L[i][j] = L[i-1][j-1]+1
-            else: 
-                L[i][j] = max(L[i-1][j], L[i][j-1]) 
-    return L[m][n] 
+    n = len(Y)   
+    if m == 0 or n == 0: 
+       return 0; 
+    elif X[m-1] == Y[n-1]: 
+       return 1 + lcs(X, Y, m-1, n-1); 
+    else: 
+       return max(lcs(X, Y, m, n-1), lcs(X, Y, m-1, n));
 X = "AGGTAB"
 Y = "GXTXAYB"
 print("Length of LCS is ", lcs(X, Y))
